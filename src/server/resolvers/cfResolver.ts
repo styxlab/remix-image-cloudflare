c304137d76eeff1fec9851f8b69fb25ed489d2b6
jj <jj>
2022-04-24 17:06:32 +0200

    log response object

--- src/server/resolvers/cfResolver.ts
+++ src/server/resolvers/cfResolver.ts
@@ -30,7 +30,7 @@ export const cloudflareResolver: Resolver = async (_asset, url, { width, height,
     },
   })
 
-  console.log('cloudflareResolver1', imageResponse.status, imageResponse)
+  console.log('cloudflareResolver1', imageResponse.status, JSON.stringify(imageResponse))
   if (imageResponse.status > 250) {
     throw new RemixImageError('cloudflareResolver failed with status ' + imageResponse.status)
   }
